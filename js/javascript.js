eb01f3eacc26a47b41e5833c8c895ae9c42476c2
nima skandar afshar <nimapa3x@gmail.com>
2023-09-02 15:11:26 +0330

    end of project

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+var number = prompt("Enter a number:");
+number = parseInt(number);
+if (number % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
